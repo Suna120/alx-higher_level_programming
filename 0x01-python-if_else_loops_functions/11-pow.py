9066217f192dbe93763bf33f3fb333b919f75cf1
Suna120 <sundaydhieu1608@gmail.com>
2021-11-17 18:39:05 -0600

    pow

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def pow(a, b):
-    pow = a*b
-    print(pow(a, b))
-    print(pow(2, 2))
-    print(pow(98, 2))
-    print(pow(98, 0))
-    print(pow(100, -2))
-    print(pow(-4, 5))
+    pow = a ^ b
+    return pow
+  
 
