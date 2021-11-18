2afc06a73a62ce33e13fd323aec9a9a4848a5ffe
Suna120 <sundaydhieu1608@gmail.com>
2021-11-17 18:49:31 -0600

    Fizz

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-pow = __import__('11-pow').pow
-
-print(pow(2, 2))
-print(pow(98, 2))
-print(pow(98, 0))
-print(pow(100, -2))
-print(pow(-4, 5))
+def pow(a, b):
+    pow = a ** b
+    return pow
   
+
