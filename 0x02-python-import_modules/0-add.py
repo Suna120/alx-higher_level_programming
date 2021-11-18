57f992078fd939912d0fb6004646623670107c70
Suna120 <sundaydhieu1608@gmail.com>
2021-11-18 14:47:45 -0600

    add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,9 @@
 from add_o import add
+
 if __name__ == "__main__":
  def add(a, b):
     a = 1
     b = 2
-    sum = a + b
-    print (sum)
- add()
+    #sum = a + b
+    print ("{} + {}= {}".format(a, b, add(a, b)))
+
