76f67b5429a324bc826e9e878a9fc1908c3a2ced
Suna120 <sundaydhieu1608@gmail.com>
2021-11-17 18:07:00 -0600

    9 last digit corrrection

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def print_last_digit(number):
+   if number < 0:
+       number *= -1
+  
+       last_digit = number % 10
+    
+       print(last_digit, end = "")
+       return number % 10
    
-    n = int(input("Enter number:"))
-    last_digit = n % 10
-    
-    print("{}".format(last_digit), end = "")
-    
-
