9226c4b8c7a556c3bcf13891177a9ef97e6ca374
Sarah Moses <92611358+Suna120@users.noreply.github.com>
2021-11-23 22:40:34 +0200

    Update 7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-def add_tuple(tuple_a=(), tuple_b=()):
-    
+def add_tuple(tuple_a=(), tuple_b=()):    
     if len(tuple_a) < 2:
         for i in range (0, 2 - len(tuple_a)):
             tuple_a = tuple_a + (0,)
@@ -8,3 +7,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
         for i in range(0, 2 - len(tuple_b)):
             tuple_b = tuple_b + (0,)
     return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+
