e49449e1c4846f06a1bdd384a2a73f4718d9ab1c
Suna120 <sundaydhieu1608@gmail.com>
2021-11-23 15:17:08 -0600

    Mm

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -7,5 +7,8 @@ def max_integer(my_list=[]):
             return max
         else:
             return None
-    my_list = [1, 90, 2, 13, 34, 5, -13, 3]
-    print("Max:{}".format(max))    
+            
+
+   
+
+    
