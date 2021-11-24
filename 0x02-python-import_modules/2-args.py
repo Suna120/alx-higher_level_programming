d99d1be524831b582d66e9d33d6adc8c9d86d0b1
Sarah Moses <92611358+Suna120@users.noreply.github.com>
2021-11-24 21:20:37 +0200

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,11 @@
-
+#!/usr/bin/python3
+import sys
+arg_c =len(sys.argv) -1
+for i in range(arg_c):
+  print("{}: {}".format(i + 1, sys.argv[i + 1]))  
+  if arg_c == 0:
+    print("0 arguments.")
+   elif arg_c == 1:
+    print("1 argument:")
+   else:
+    print("{}:{}".format(i+1, sys.argv[i + 1]))
