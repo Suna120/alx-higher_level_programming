531692b0fe3f784268e1e2455c4650d72b996a13
Sarah Moses <92611358+Suna120@users.noreply.github.com>
2021-11-24 20:53:19 +0200

    Update 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,1 +1,14 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, div, mul
+if __name__ == "__main__"
+a = 10
+b = 5
+for a + b:
+  return add(a, b)
+for a - b:
+  return sub(a, b)
+for a / b:
+  return div(a, b)
+for a * b:
+  return mul(a, b)
 
