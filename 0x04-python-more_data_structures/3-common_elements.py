e9eefdf5b29fcf6711b542651659458cc1a98e89
Suna120 <sundaydhieu1608@gmail.com>
2021-11-26 17:22:42 -0600

    common elements1

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-set_1 = { "Python", "C", "Javascript" }
-set_2 = { "Bash", "C", "Ruby", "Perl"}
-
 def common_elements(set_1, set_2):
-    set_c = [value for value in set_1 if value in set_2]
-    return set_c
-x = common_elements(set_1, set_2)
-print(x, end=" ")
+  return set_1.intersection(set_2)
