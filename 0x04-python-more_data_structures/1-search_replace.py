8471c1777ed553f5c526c261eb7c9f7d2a5edf50
Suna120 <sundaydhieu1608@gmail.com>
2021-11-26 16:55:22 -0600

    Replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = []
+    for i in range(len(new_list)) :
+        new_list.get(i)
+        new_list[i] = new_list[i] * 2
+    return new_list
+        
+    
