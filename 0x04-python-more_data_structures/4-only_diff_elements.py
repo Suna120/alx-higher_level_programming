2cdb0d3ca772bc146861260622dc683e0d50f50d
Suna120 <sundaydhieu1608@gmail.com>
2021-11-26 17:34:32 -0600

    symmetric difference2

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -3,5 +3,6 @@ def only_diff_elements(set_1, set_2):
     set_1 = set([])
     set_2 = set([])
  #symmetric difference
-    c = set_1 ^ set_2
-    return c
+    ''' c = set_1 ^ set_2
+    return c'''
+    return set_1.symmetric_difference(set_2)
