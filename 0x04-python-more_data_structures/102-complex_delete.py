b718594d53b0a68751f1e87c43afbd3852cb51a8
Sarah Moses <92611358+Suna120@users.noreply.github.com>
2021-11-27 03:41:28 +0200

    Update 102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    x = dict(a_dictionary)
-    del x[value]
-    return x
+    copy = a_dictionary.copy()
+    del copy[value]
+    return copy
