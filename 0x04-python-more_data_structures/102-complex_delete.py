0932374b1c08a4bf866927ff30a22ffe8bfdbbd9
Suna120 <sundaydhieu1608@gmail.com>
2021-11-26 19:04:23 -0600

    complex delete dictionary value

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    if value in a_dictionary:
+        del a_dictionary[value]
+        print(a_dictionary)
+    else:
+        print(a_dictionary)
