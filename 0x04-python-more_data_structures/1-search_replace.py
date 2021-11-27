748b4f99c795d474b8933cd9542e33a274059ddc
Sarah Moses <92611358+Suna120@users.noreply.github.com>
2021-11-27 03:30:39 +0200

    Update 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,10 +2,16 @@
 def search_replace(my_list, search, replace):
     new_list = []
     i = 0
+    """
+    first soln to task. 4/9.
+    for i in range(len(new_list)) :
+        new_list.get(i)
+        new_list[i] = new_list[i] * 2
+    return new_list"""
     while i != len(my_list):
         if my_list[i] != search:
             new_list.append(my_list[i])
-            else:
-                new_list.append(replace)
-                i += 1
-                return new_list
+        else:
+            new_list.append(replace)
+        i += 1
+    return new_list
