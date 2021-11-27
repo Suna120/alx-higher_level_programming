58a64c83b82e0f7dbf7c485c8a84568a2769b62b
Sarah Moses <92611358+Suna120@users.noreply.github.com>
2021-11-27 03:22:34 +0200

    Update 100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    sum = 0
-    for sub in my_list:
-        for i in sub:
-            sum = sum + i
-    result = sum / len (my_list)
-    if len(my_list) >= 0:
-        print(result)
-    elif len < 0:
-        print(0)    
-        
+    average = [sum(x) / len(x) for x in zip(*my_list)]
+    if my_list:
+        return average(my_list)
+    else:
+        return 0         
