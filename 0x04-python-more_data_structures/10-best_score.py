4d0cac0e0323e6a2d0690647d2d3958ec65411e7
Sarah Moses <92611358+Suna120@users.noreply.github.com>
2021-11-27 03:33:52 +0200

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    #best = max(a_dictionary.keys(), key=lambda k: a_dictionary[k])
     if a_dictionary:
         return max(a_dictionary, key = a_dictionary.get)
     else:
