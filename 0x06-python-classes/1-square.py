34a8f8669148f6b9f39e78c285844fb65c1130a9
Sarah Moses <92611358+Suna120@users.noreply.github.com>
2021-12-08 23:10:38 +0200

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-"""Define Square class"""
+"""Define Square class that defines a square by: (based on 0-square.py)"""
 
 
 class Square:
-    __size = size
+    def size(self):
+        """
+        initialize the private instance attribute
+        """
+        return self.__size 
