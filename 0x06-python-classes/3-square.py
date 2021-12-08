bae066a90b152df8dd44e4b2eb8c131f560a1c10
Sarah Moses <92611358+Suna120@users.noreply.github.com>
2021-12-08 23:31:12 +0200

    Update 3-square.py
    area

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,1 +1,18 @@
+#!/usr/bin/python3
+"""Square class that defines a square"""
 
+
+class Square:
+    def __init__(self, size=0):
+        """
+        initialize the private instance attribute
+        """
+        self.__size = size
+        if size not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+        def area(self):
+            """function defines area of Square"""
+            return self.__size ** 2
