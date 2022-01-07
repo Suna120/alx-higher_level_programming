56394b8d7aaa419da16c7bdd4bcca35f6c3239e4
Suna120 <sundaydhieu1608@gmail.com>
2022-01-07 10:41:56 -0600

    models

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+class Base():
+    def __init__(self, id=None):
+        self.__nb_objects = 0
+        self.id =id
+        if id !=None:
+            id == id
+        else:
+            __nb_objects += 1
+            id = __nb_objects
+
+
